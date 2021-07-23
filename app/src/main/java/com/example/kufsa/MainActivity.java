6af31a30810e08f680e721e67c12655bfefa8154
David-tawil <david8tawil@gmail.com>
2021-07-23 19:03:40 +0300

    fix code with drawer and delete button navigation

--- app/src/main/java/com/example/kufsa/MainActivity.java
+++ app/src/main/java/com/example/kufsa/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.kufsa;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import static androidx.navigation.ui.NavigationUI.setupActionBarWithNavController;
 
@@ -21,20 +22,26 @@ public class  MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.nav_activity_main);
+        setContentView(R.layout.activity_main);
 
 
-        BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav_view);
+        //BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav_view);
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.;
-        appBarConfiguration = new AppBarConfiguration.Builder(R.id.marketplace_fragment, R.id.my_games_fragment, R.id.my_account_fragment, R.id.signedInAccountFragment).build();
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.marketplace_fragment, R.id.my_games_fragment, R.id.my_account_fragment, R.id.signedInAccountFragment)
+                .setDrawerLayout(drawer)
+                .build();
+        NavigationView navView = findViewById(R.id.nav_view);
+
         setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(bottomNavView, navController);
+        NavigationUI.setupWithNavController(navView, navController);
+
     }
 
     @Override
     public boolean onSupportNavigateUp() {
-        return NavigationUI.navigateUp(navController, appBarConfiguration);
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 }
