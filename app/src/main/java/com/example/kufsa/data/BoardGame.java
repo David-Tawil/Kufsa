32173a2ca6da9301ae07f92ecafff43edf8cd436
eldar101 <eldar101@gmail.com>
2021-08-21 23:56:19 +0300

    Fixed landscape layout and more documentation

--- app/src/main/java/com/example/kufsa/data/BoardGame.java
+++ app/src/main/java/com/example/kufsa/data/BoardGame.java
@@ -34,10 +34,11 @@ public class BoardGame implements Parcelable {
     private int totalThreeStar;
     private int totalFourStar;
     private int totalFiveStar;
+    private String id;
+
 
     public BoardGame() {// Needed for Firebase
     }
-
 
     /**
      * Board game constructor
@@ -74,6 +75,9 @@ public class BoardGame implements Parcelable {
         this.totalFiveStar = totalFiveStar;
     }
 
+    /**
+     * @param in parcel object that contains all board game info
+     */
     protected BoardGame(Parcel in) {
         id = in.readString();
         name = in.readString();
@@ -103,153 +107,336 @@ public class BoardGame implements Parcelable {
         return CREATOR;
     }
 
+    /**
+     * Retrieves a game's Firebase ID.
+     *
+     * @return The game's ID in the form of a string.
+     */
     public String getId() {
         return id;
     }
 
-
-    private String id;
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public Float getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(Float difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public int getMaxNumOfPlayers() {
-        return maxNumOfPlayers;
-    }
-
-    public void setMaxNumOfPlayers(int maxNumOfPlayers) {
-        this.maxNumOfPlayers = maxNumOfPlayers;
-    }
-
-    public int getMinNumOfPlayers() {
-        return minNumOfPlayers;
-    }
-
-    public void setMinNumOfPlayers(int minNumOfPlayers) {
-        this.minNumOfPlayers = minNumOfPlayers;
-    }
-
-    public int getPlayingTime() {
-        return playingTime;
-    }
-
-    public void setPlayingTime(int playingTime) {
-        this.playingTime = playingTime;
-    }
-
-    public int getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(int releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    public int getMinAge() {
-        return minAge;
-    }
-
-    public void setMinAge(int minAge) {
-        this.minAge = minAge;
-    }
-
+    /**
+     * Set a game's Firebase ID.
+     *
+     * @param id The ID you wish to set.
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves a game's image URL.
+     *
+     * @return The game's image URL as a string.
+     */
+    public String getImg() {
+        return img;
+    }
+
+    /**
+     * Set a game's image URL.
+     *
+     * @param img The URL you wish to set for the game's image
+     */
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    /**
+     * Retrieves a game's publisher.
+     *
+     * @return The game's publisher as a string.
+     */
+    public String getPublisher() {
+        return publisher;
+    }
+
+    /**
+     * Set a game's publisher name.
+     *
+     * @param publisher The name of the publisher you wish to set.
+     */
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    /**
+     * Retrieves a game's difficulty.
+     *
+     * @return The game's difficulty as a float.
+     */
+    public Float getDifficulty() {
+        return difficulty;
+    }
+
+    /**
+     * Set a game's difficulty level in the form of a float.
+     *
+     * @param difficulty The game's difficulty level you wish to set.
+     */
+    public void setDifficulty(Float difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    /**
+     * Retrieves a game's maximal number of players.
+     *
+     * @return The game's maximal number of players as an int.
+     */
+    public int getMaxNumOfPlayers() {
+        return maxNumOfPlayers;
+    }
+
+    /**
+     * Set a game's maximal numbeer of players.
+     *
+     * @param maxNumOfPlayers The game's number of players you wish to set.
+     */
+    public void setMaxNumOfPlayers(int maxNumOfPlayers) {
+        this.maxNumOfPlayers = maxNumOfPlayers;
+    }
+
+    /**
+     * Retrieves a game's minimal number of players.
+     *
+     * @return The game's minimal number of players as an int.
+     */
+    public int getMinNumOfPlayers() {
+        return minNumOfPlayers;
+    }
+
+    /**
+     * Set a game's minimal numbeer of players.
+     *
+     * @param minNumOfPlayers The game's number of players you wish to set.
+     */
+    public void setMinNumOfPlayers(int minNumOfPlayers) {
+        this.minNumOfPlayers = minNumOfPlayers;
+    }
+
+    /**
+     * Retrieves a game's playing time.
+     *
+     * @return The game's playing time as an int.
+     */
+    public int getPlayingTime() {
+        return playingTime;
+    }
+
+    /**
+     * Set a game's playing time.
+     *
+     * @param playingTime The game's playing time you wish to set.
+     */
+    public void setPlayingTime(int playingTime) {
+        this.playingTime = playingTime;
+    }
+
+    /**
+     * Retrieves a game's release year.
+     *
+     * @return The game's release year as an int.
+     */
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    /**
+     * Set a game's release year.
+     *
+     * @param releaseYear The game's release year you wish to set.
+     */
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    /**
+     * Retrieves a game's minimal age requirement.
+     *
+     * @return The game's minimal age requirement as an int.
+     */
+    public int getMinAge() {
+        return minAge;
+    }
+
+    /**
+     * Set a game's minimal age requirement.
+     *
+     * @param minAge The game's minimal playing age you wish to set.
+     */
+    public void setMinAge(int minAge) {
+        this.minAge = minAge;
+    }
+
+    /**
+     * Retrieves a game's name.
+     *
+     * @return The game's name as a string.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set a game's name.
+     *
+     * @param name The name you wish to set for the game
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Retrieves a game's description.
+     *
+     * @return The game's description in the form of a String.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set a game's description.
+     *
+     * @param description The description you wish to set for the game.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Retrieves a game's average review score.
+     *
+     * @return The game's average review score in the form of a float.
+     */
     public float getAverageReviewScore() {
         return averageReviewScore;
     }
 
+    /**
+     * Set a game's average review score.
+     *
+     * @param averageReviewScore The score you wish to set for the game.
+     */
     public void setAverageReviewScore(float averageReviewScore) {
         this.averageReviewScore = averageReviewScore;
     }
 
+    /**
+     * Retrieves a game's number of total reviews.
+     *
+     * @return The game's total reviews in the form of an int.
+     */
     public int getTotalReviews() {
         return totalReviews;
     }
 
+    /**
+     * Set a game's num of total reviews.
+     *
+     * @param totalReviews The number of reviews you wish to set for the game.
+     */
     public void setTotalReviews(int totalReviews) {
         this.totalReviews = totalReviews;
     }
 
+    /**
+     * Retrieves a game's number of one star reviews.
+     *
+     * @return The game's number of one star reviews in the form of an int.
+     */
     public int getTotalOneStar() {
         return totalOneStar;
     }
 
+    /**
+     * Set a game's num of one star reviews.
+     *
+     * @param totalOneStar The number of one star reviews you wish to set for the game.
+     */
     public void setTotalOneStar(int totalOneStar) {
         this.totalOneStar = totalOneStar;
     }
 
+    /**
+     * Retrieves a game's number of two star reviews.
+     *
+     * @return The game's number of two star reviews in the form of an int.
+     */
     public int getTotalTwoStar() {
         return totalTwoStar;
     }
 
+    /**
+     * Set a game's num of total two star reviews.
+     *
+     * @param totalTwoStar The number of two star reviews you wish to set for the game.
+     */
     public void setTotalTwoStar(int totalTwoStar) {
         this.totalTwoStar = totalTwoStar;
     }
 
+    /**
+     * Retrieves a game's number of three star reviews.
+     *
+     * @return The game's number of three star reviews in the form of an int.
+     */
     public int getTotalThreeStar() {
         return totalThreeStar;
     }
 
+    /**
+     * Set a game's number of three star reviews.
+     *
+     * @param totalThreeStar The number of three star reviews you wish to set for the game.
+     */
     public void setTotalThreeStar(int totalThreeStar) {
         this.totalThreeStar = totalThreeStar;
     }
 
+    /**
+     * Retrieves a game's number of four star reviews.
+     *
+     * @return The game's number of four star reviews in the form of an int.
+     */
     public int getTotalFourStar() {
         return totalFourStar;
     }
 
+    /**
+     * Set a game's number of four star reviews.
+     *
+     * @param totalFourStar The number of four star reviews you wish to set for the game.
+     */
     public void setTotalFourStar(int totalFourStar) {
         this.totalFourStar = totalFourStar;
     }
 
+    /**
+     * Retrieves a game's number of five star reviews.
+     *
+     * @return The game's number of five star reviews in the form of an int.
+     */
     public int getTotalFiveStar() {
         return totalFiveStar;
     }
 
+    /**
+     * Set a game's number of five star reviews.
+     *
+     * @param totalFiveStar The number of five star reviews you wish to set for the game.
+     */
     public void setTotalFiveStar(int totalFiveStar) {
         this.totalFiveStar = totalFiveStar;
     }
 
+    /**
+     * Write all board game info into a parcel obj.
+     *
+     * @param dest  destionation parcel.
+     * @param flags flags meant to initialize.
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(id);
@@ -277,6 +464,11 @@ public class BoardGame implements Parcelable {
         dest.writeInt(totalFiveStar);
     }
 
+    /**
+     * Describe a game's content.
+     *
+     * @return 0
+     */
     @Override
     public int describeContents() {
         return 0;
